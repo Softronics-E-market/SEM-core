291b258a61011bbec9c4fa7bc30a17b3bf3213b9
Admin-softronics <softronics.techlegend@gmail.com>
2024-02-21 22:45:26 +0530

    chore: enhanced the package json

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,18 @@
-import { ApolloServer } from 'apollo-server-express';
-import { ApolloServer as ApolloServerLambda } from 'apollo-server-lambda';
-import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
-import http from 'http';
-import express from 'express';
-import cors from 'cors';
-import typeDefs from './graphql/typeDefs/typeDefs.js';
-import resolvers from './graphql/resolvers/resolver.js';
+import { ApolloServer } from 'apollo-server-micro';
+import typeDefs from './graphql/typeDefs/typeDefs';
+import resolvers from './graphql/resolvers/resolver';
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-const httpServer = http.createServer(app);
-
-const startApolloServer = async (app, httpServer) => {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-    cache: 'bounded',
-  });
-
-  await server.start();
-  server.applyMiddleware({ app });
-};
-
-app.listen(6060, () =>
-  console.log(
-    'Hosted url is -> http://localhost:6060/graphql, web -> https://graphql-js-hazel.vercel.app/graphql',
-  ),
-);
-
-startApolloServer(app, httpServer);
-
-const server = new ApolloServerLambda({
+const server = new ApolloServer({
   typeDefs,
   resolvers,
   introspection: true,
   cache: 'bounded',
 });
 
-const handler = server.createHandler({ cors: { origin: '*' } } as any);
+try {
+  server.start();
+} catch (error) {
+  console.log("error : ", error)
+}
 
-export default handler;
+export default server.createHandler({path: "/api/graphql"});
